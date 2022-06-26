e2c11dc3bc32794336130807367b240dec10e12f
MarekDrag <70659067+MarekDrag@users.noreply.github.com>
2022-06-26 12:58:33 +0200

    prepare to deploy 3 fix problem with api

--- frontend/src/pages/login.js
+++ frontend/src/pages/login.js
@@ -1,4 +1,4 @@
-import React, { useContext, useEffect, useState } from 'react';
+import React, { useContext, useState } from 'react';
 import { Link, useNavigate } from 'react-router-dom';
 import styled from 'styled-components';
 import axios from '../axios';
